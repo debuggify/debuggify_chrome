f75ecf9e131a32a0fe812b8b2f2bbda674d24b19
Ankur Agarwal <signup.ankur@gmail.com>
2012-10-16 21:14:36 +0530

    Saving work

--- app/log.js
+++ app/log.js
@@ -1,4 +1,38 @@
-console.log("debuggify loaded")
-console.log(typeof debuggify);
-console.log(document.location)
-console.log(window)
+console.log('debuggify loading');
+
+
+// chrome.extension.sendRequest({method: "getLocalStorage", key: "apikey"}, function(response) {
+//   console.log(response.data);
+// });
+
+// chrome.extension.sendRequest({method: "getLocalStorage",args:  ["apikey"] }, function(response) {
+//   console.log(response.data);
+// });
+
+// chrome.extension.sendRequest({method: "isEnabledForURL",args:  [document.location.hostname] }, function(response) {
+//   console.log(response.data);
+// });
+
+function injectScript(url) {
+
+  // Inject the script tag
+  (function() {
+    var dfy = document.createElement('script');
+    dfy.type = 'text/javascript';
+    dfy.async = false;
+    dfy.src = url;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(dfy, s);
+  })();
+}
+
+chrome.extension.sendRequest({method: "processTab", args: [document.location] }, function(response) {
+
+  console.log(response.data);
+
+  if(!response.data) {
+    return false;
+  }
+
+  injectScript(response.data);
+});
